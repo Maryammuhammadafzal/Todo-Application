22b1142096e71861c8ad346fc65fec84b3dc3332
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-08-21 00:42:14 +0500

    Todo-List

--- script.js
+++ script.js
@@ -20,8 +20,55 @@
 // });
 
 
+
 function showPrompt(){
     var modalDiv = document.getElementById('popup');
-     modalDiv.className = "modal-popup";
-
+    modalDiv.className = "modal";
+    modalDiv.removeAttribute("style")
 }
+
+
+document.getElementById("add-btn").addEventListener('click' , function(){
+    var taskText = document.getElementById("task-input-text").value;
+    if (taskText === "") return; //prevent adding empty tasks
+
+    // Creating Checkbox button
+    var listItemCheckbox = document.createElement('button');
+    listItemCheckbox.classList.add('check-box');
+
+    // Create text Element 'p' and adding text
+    var text = document.createElement('p');
+    text.textContent = taskText.value;
+
+    //Get ul list id and create list items
+    var taskList = document.getElementById("task-list");
+    var listItem = document.createElement('li');
+
+    //Adding checkbox and text in list item
+    // listItem.textContent = listItemCheckbox;
+    listItem.textContent = text;
+  
+    //Adding list item li into the task list
+    taskList.appendChild(listItemCheckbox);
+    taskList.appendChild(listItem);
+    console.log(taskList);
+    
+
+    // taskList.innerHTML = listItem.innerHTML;
+    // console.log(listItem);
+    
+    listItemCheckbox.addEventListener('click', function(){
+        taskList.ariaChecked(listItem);
+    })
+
+
+    listItem.appendChild(listItemCheckbox);
+    taskList.appendChild(listItem);
+
+
+    document.getElementById('task-input-text').value = ''; // Clear the input
+
+    var modalDiv = document.getElementById('popup');
+    modalDiv.style.display = "none"
+
+})
